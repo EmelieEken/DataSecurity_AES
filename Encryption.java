3de3e48b5a6dbe9c8cbf1305b1666a9b943fb740
EmelieEken <emeeke@student.chalmers.se>
2017-05-14 15:25:17 +1000

    Changed to work with Block

--- Encryption.java
+++ Encryption.java
@@ -1,6 +1,9 @@
+//package data_security_assignment_2;
+//import data_security_assignment_2.*;
+
 public class Encryption{
-    //private Block[] blocks; //Array of blocks
-    private int[][][] blocks;
+    private Block[] blocks; //Array of Blocks to be encrypted (from text)
+    //private int[][][] blocks;
     private int modeOfOperation;
     private int transmissionSize;
     private int[] initVector;
@@ -22,7 +25,7 @@ public class Encryption{
     }
 
     public String Encrypt(int[] text, int[] key) {
-        encryptionFunctions encr = new encryptionFunctions();
+        //encryptionFunctions encr = new encryptionFunctions();
 
         //Print text
         System.out.print("Text: \n [ " );
@@ -44,17 +47,18 @@ public class Encryption{
 
         //Create blocks
         System.out.println("Dividing into blocks");
-        blocks = new int[text.length/16][4][4]; //Initialise array of textblocks for encryption
+        blocks = new Block[text.length/16]; //Initialise array of textblocks for encryption
         for (int i=0; i<text.length/16; i++) {
             int n=0;
-            //blocks[i] = new int[4][4];
+            int[] textToBlock = new int[16];
             for (int j=0; j<4; j++) {
                 for (int k=0; k<4; k++) {
-                    blocks[i][k][j] = text[16*i+n];
-                    System.out.print(blocks[i][k][j] + " ");
+                    textToBlock[n] = text[16*i+n];
+                    System.out.print(textToBlock[n] + " ");
                     n++;
                 }
             }
+            blocks[i] = new Block(textToBlock);
             System.out.println();
         }
 
@@ -62,6 +66,7 @@ public class Encryption{
         if (modeOfOperation == 0) { //ECB, Electronic code book. Same key used for all blocks
             for (int i = 0; i<text.length/16; i++) {
                 EncryptBlock(blocks[i], key);
+                //blocks[i].toString();
             }
 
         }
@@ -69,48 +74,62 @@ public class Encryption{
         //Print blocks
         System.out.print("\nAfter encryption: \n");
         for (int i=0; i<text.length/16; i++) {
-            System.out.print("{ ");
-            for (int j=0; j<4; j++) {
-                for (int k=0; k<4; k++) {
-                    System.out.print(blocks[i][k][j] + " ");
-                }
-                System.out.println();
-            }
-            System.out.print("} \n");
+            blocks[i].toString();
         }
             
 
         //Encrypt every block following the given modeOfOperation (call functions below)
+        String cipherText = "";
+        for (int i=0; i<text.length/16; i++) {
+            cipherText += blocks[i].toStringOneLine(); 
+        }
 
-        return "*"; //Return encrypted 
+        return cipherText; //Return encrypted 
     }
 
     //create functions for all modeOfOperation
 
     //ECB
-    private static void EncryptBlock(int[][] block, int[] key) {
+    private static void EncryptBlock(Block block, int[] key) {
 
         //Expand key
         int[][] expandedKey = encryptionFunctions.keyExpansion(key);
 
-        encryptionFunctions.addRoundKey(block, key); //Add round key (original key) before going into first round
+        block.addRoundKey(key); //Add round key (original key) before going into first round
 
         for (int i = 0; i<9; i++) { //Do 9 rounds: byte sustitution, shift rows, mix col, add round key
-            encryptionFunctions.substituteBytes(block);
-            //encryptionFunctions.shiftRows(block);
-            //encryptionFunctions.mixCols(block);
+            
+            block.substituteBytes();
+            System.out.println("\n After byte substitution \n" + block.toString());
+            //block.shiftRows();
+            //block.mixCols();
             int[] currentKey = new int[16];
+            //System.out.print("\nCurrent key");
             for (int j = 0; j<16; j++) {
 
-                currentKey[j] = expandedKey[(i+1)*4][j%4]; 
-                System.out.print(currentKey[j] + " ");
+                currentKey[j] = expandedKey[(i+1)*4 + j/4][j%4]; 
+                //System.out.print(currentKey[j] + " ");
             }
-            encryptionFunctions.addRoundKey(block,currentKey);
+            block.addRoundKey(currentKey);
+            System.out.println("\n After addRoundKey \n" + block.toString());
         }
+
+        //Round 10 
+        System.out.println("-----Round 10-----");
+        block.substituteBytes();
+        System.out.println("\n After byte substitution \n" + block.toString());
+        //block.shiftRows();
+        int[] currentKey = new int[16];
+        for (int j = 0; j<16; j++) {
+            currentKey[j] = expandedKey[(9+1)*4 + j/4][j%4]; 
+            //System.out.print(currentKey[j] + " ");
+        }
+        block.addRoundKey(currentKey);
+        System.out.println("\n After addRoundKey \n" + block.toString());
     }
 
     public static void main(String[] args) {
-        int[] in = {0,0};
+        int[] in = {0};
         int[] plainText = {0x56, 0xA2, 0x5F, 0x9F, 0x5F, 0x5F, 0x8F, 0x5F, 0x5F, 0x52, 0x5F, 0x5F, 0x5F, 0x5F, 0x4F, 0x5F, 0x46, 0xA2, 0x5F, 0x9F, 0x5F, 0x5F, 0x8F, 0x5F, 0x5F, 0x52, 0x5F, 0x5F, 0x5F, 0x5F, 0x4F, 0x5F};
         int[] key = {0xA2, 0x56, 0xA2, 0x56, 0xA2, 0x56, 0xA2, 0x56, 0xA2, 0x56, 0xA2, 0x56, 0xA2, 0x56, 0xA2, 0xE5};
         Encryption encrypt = new Encryption(0,0,in);
