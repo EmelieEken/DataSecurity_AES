c615329d95155b12c6e05fa52c02c2bd8c2ad1fd
EmelieEken <emeeke@student.chalmers.se>
2017-05-17 19:36:32 +1000

    CFB implemented

--- Encryption.java
+++ Encryption.java
@@ -32,8 +32,9 @@ public class Encryption{
             System.exit(0);
         }
 
+        //Check Initialisation vector
         if (modeOfOperation == 0 && (initVector.length != 1 || initVector[0] != 0)) {
-            System.out.println(initVector[0]);
+            //System.out.println(initVector[0]);
             System.out.println("Initialisation vector must be set to 0 for ECB mode");
             System.exit(0);
         }
@@ -43,21 +44,35 @@ public class Encryption{
             System.exit(0);
         }
 
-        //Create blocks (array of Block)
-        blocks = new Block[text.length/16]; //Initialise array of textblocks for encryption
-        for (int i=0; i<text.length/16; i++) {
-            int n=0;
-            int[] textToBlock = new int[16];
-            for (int j=0; j<4; j++) {
-                for (int k=0; k<4; k++) {
-                    textToBlock[n] = text[16*i+n];
-                    //System.out.print(textToBlock[n] + " ");
-                    n++;
-                }
-            }
-            blocks[i] = new Block(textToBlock);
-            //System.out.println();
+        //Check Transmission size
+        if (modeOfOperation != 1 && transmissionSize != 0) {
+            System.out.println("Transmission size must be set to 0 for ECB, CBC, and OFB mode");
+            System.exit(0);
+        } 
+
+        if (modeOfOperation == 1 && (transmissionSize < 1 || transmissionSize > 16)) {
+            System.out.println("Transmission size must be in the interval 1-16 bytes");
+            System.exit(0);
         }
+
+        //Create blocks (array of Block), not for CFB
+        if (modeOfOperation != 1) {
+            blocks = new Block[text.length/16]; //Initialise array of textblocks for encryption
+            for (int i=0; i<text.length/16; i++) {
+                int n=0;
+                int[] textToBlock = new int[16];
+                for (int j=0; j<4; j++) {
+                    for (int k=0; k<4; k++) {
+                        textToBlock[n] = text[16*i+n];
+                        //System.out.print(textToBlock[n] + " ");
+                        n++;
+                    }
+                }
+                blocks[i] = new Block(textToBlock);
+                //System.out.println();
+            }
+        }
+        
 
 
         switch (modeOfOperation) { //ECB, Electronic code book. Same key used for all blocks
@@ -66,7 +81,23 @@ public class Encryption{
                 break;
 
             case 1: //CFB
-            //EncryptBlockCFB();
+                EncryptBlockCFB(text, key);
+                //Create blocks for output
+                blocks = new Block[text.length/16]; //Initialise array of textblocks for encryption
+                for (int i=0; i<text.length/16; i++) {
+                    int n=0;
+                    int[] textToBlock = new int[16];
+                    for (int j=0; j<4; j++) {
+                        for (int k=0; k<4; k++) {
+                            textToBlock[n] = text[16*i+n];
+                            //System.out.print(textToBlock[n] + " ");
+                            n++;
+                        }
+                    }
+                    blocks[i] = new Block(textToBlock);
+                    //System.out.println();
+                }
+
                 break;
             case 2:  //CBC
             	EncryptBlockCBC(blocks, key);
@@ -120,8 +151,64 @@ public class Encryption{
 //        blocks = newBlocks;
     }
 
-    private void EncryptBlockCFB() {
+    private void EncryptBlockCFB(int[] text ,int[] key) {
 
+        Block initBlock = new Block(this.initVector);
+        //System.out.print("\n" + initBlock.toString());
+
+        int[] currentText = new int[transmissionSize];
+
+        //Divide plaintext into arrays of length transmissionSize
+        for(int i=0; i<text.length/transmissionSize; i++) {
+            initBlock.encrypt(key);
+            int[] selectedBytes = selectSBytes(initBlock);//Select transmissionSize bytes
+            int[] c_i = new int[transmissionSize]; //Ciphertext to be used in next round in shift register
+            for (int j=0; j<transmissionSize; j++) {
+                text[i*transmissionSize + j] = text[i*transmissionSize + j]^selectedBytes[j]; //XOR with plaintext 
+                c_i[j] = text[i*transmissionSize + j]; 
+            }
+            initBlock = new Block(shiftRegister(initBlock, c_i)); //Prepare for next round
+
+        }
+        //for (int i=0; i<blocks.length; i++) {
+             //Encrypt Nonce (= initVector)
+
+            //blocks[i] = blocks[i].add(initBlock); //XOR first block and the encrypted nonce 
+        //}
+
+    }
+
+    private int[] selectSBytes(Block block) {
+        //System.out.println(block.toString());
+        int[] selectedBytes = new int[transmissionSize];
+        for (int i=0; i<transmissionSize; i++) {
+            selectedBytes[i] = block.get(i);
+            //System.out.println( String.format("%02X",selectedBytes[i]) + " ");
+        }
+
+        return selectedBytes;
+    }
+
+    private int[] shiftRegister(Block previousEncrIV, int[] text) {
+        //System.out.println("\n\n" + previousEncrIV.toString());
+        //System.out.println("Ciphertext ");
+        //for (int i=0; i<text.length; i++) {
+        //    System.out.print(String.format("%02X",text[i]) + " ");;
+        //}
+
+        //System.out.println("\nnewIV ");
+
+        int[] newIV = new int[16]; //16 = IV size
+        for (int i=0; i<16-transmissionSize; i++) {
+            newIV[i] = previousEncrIV.get(i+transmissionSize);//Get Least significant bits (to the right)
+            //System.out.print(String.format("%02X",newIV[i]) + " ");
+        }
+        for (int i=16-transmissionSize; i<16; i++) {
+            newIV[i] = text[i-(16-transmissionSize)]; 
+            //System.out.print(String.format("%02X",newIV[i]) + " ");
+        }
+
+        return newIV;
     }
 
     private void EncryptBlockOFB(Block[] blocks, int[] key) {
