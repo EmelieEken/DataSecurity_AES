5a05c86fb55d226aa8464ce2274f4f0de79ab345
EmelieEken <emeeke@student.chalmers.se>
2017-05-17 21:09:17 +1000

    Prepared for CFB

--- Decryption.java
+++ Decryption.java
@@ -31,6 +31,7 @@ public class Decryption{
             System.exit(0);
         }
 
+        //Check Initialisation vector
         if (modeOfOperation == 0 && (initVector.length != 1 || initVector[0] != 0)) {
             //System.out.println(initVector[0]);
             System.out.println("Initialisation vector must be set to 0 for ECB mode");
@@ -41,19 +42,32 @@ public class Decryption{
             System.out.println("Initialisation vector must be 16 bytes long for CBC, CFB, and OFB mode");
             System.exit(0);
         }
+
+        //Check Transmission size
+        if (modeOfOperation != 1 && transmissionSize != 0) {
+            System.out.println("Transmission size must be set to 0 for ECB, CBC, and OFB mode");
+            System.exit(0);
+        } 
+
+        if (modeOfOperation == 1 && (transmissionSize < 1 || transmissionSize > 16)) {
+            System.out.println("Transmission size must be in the interval 1-16 bytes");
+            System.exit(0);
+        }
         
-        //Create blocks
-        blocks = new Block[text.length/16]; //Initialise array of textblocks for Decryption
-        for (int i=0; i<text.length/16; i++) {
-            int n=0;
-            int[] textToBlock = new int[16];
-            for (int j=0; j<4; j++) {
-                for (int k=0; k<4; k++) {
-                    textToBlock[n] = text[16*i+n];
-                    n++;
+        //Create blocks (array of Block), not for CFB
+        if (modeOfOperation != 1) {
+            blocks = new Block[text.length/16]; //Initialise array of textblocks for Decryption
+            for (int i=0; i<text.length/16; i++) {
+                int n=0;
+                int[] textToBlock = new int[16];
+                for (int j=0; j<4; j++) {
+                    for (int k=0; k<4; k++) {
+                        textToBlock[n] = text[16*i+n];
+                        n++;
+                    }
                 }
+                blocks[i] = new Block(textToBlock);
             }
-            blocks[i] = new Block(textToBlock);
         }
 
 
@@ -62,7 +76,21 @@ public class Decryption{
                 DecryptBlockECB(blocks, key);
                 break;
             case 1: //CFB
-                //DecryptBlockCFB();
+                DecryptBlockCFB(text, key);
+                blocks = new Block[text.length/16]; //Initialise array of textblocks for encryption
+                for (int i=0; i<text.length/16; i++) {
+                    int n=0;
+                    int[] textToBlock = new int[16];
+                    for (int j=0; j<4; j++) {
+                        for (int k=0; k<4; k++) {
+                            textToBlock[n] = text[16*i+n];
+                            //System.out.print(textToBlock[n] + " ");
+                            n++;
+                        }
+                    }
+                    blocks[i] = new Block(textToBlock);
+                    //System.out.println();
+                }
                 break;
             case 2:  //CBC
                 DecryptBlockCBC(blocks, initVector);
@@ -126,8 +154,64 @@ public class Decryption{
 //    		System.out.println("new Block:\n"+blocks[i].toString());
     		c_i_1 = new Block(c_i);
     	}
+    }
 
-    private void DecryptBlockCFB() {
+    private void DecryptBlockCFB(int[] text ,int[] key) {
+        Block initBlock = new Block(this.initVector);
+        //System.out.print("\n" + initBlock.toString());
+
+        int[] currentText = new int[transmissionSize];
+
+        //Divide plaintext into arrays of length transmissionSize
+        for(int i=0; i<text.length/transmissionSize; i++) {
+            initBlock.encrypt(key);
+            int[] selectedBytes = selectSBytes(initBlock);//Select transmissionSize bytes
+            int[] c_i = new int[transmissionSize]; //Ciphertext to be used in next round in shift register
+            for (int j=0; j<transmissionSize; j++) {
+                c_i[j] = text[i*transmissionSize + j]; //Save ciphertext bit to use in shift register
+                System.out.print( String.format("%02X",c_i[i]) + " ");
+                text[i*transmissionSize + j] = text[i*transmissionSize + j]^selectedBytes[j]; //XOR with plaintext 
+                System.out.print( String.format("%02X",c_i[i]) + " ");
+                
+            }
+            initBlock = new Block(shiftRegister(initBlock, c_i)); //Prepare for next round
+
+        }
+
+    }
+
+    private int[] selectSBytes(Block block) {
+        //System.out.println(block.toString());
+        int[] selectedBytes = new int[transmissionSize];
+        for (int i=0; i<transmissionSize; i++) {
+            selectedBytes[i] = block.get(i);
+            //System.out.println( String.format("%02X",selectedBytes[i]) + " ");
+        }
+
+        return selectedBytes;
+    }
+
+    private int[] shiftRegister(Block previousEncrIV, int[] text) {
+        //System.out.println("\n\n" + previousEncrIV.toString());
+        //System.out.println("Ciphertext ");
+        //for (int i=0; i<text.length; i++) {
+        //    System.out.print(String.format("%02X",text[i]) + " ");;
+        //}
+
+        //System.out.println("\nnewIV ");
+
+        int[] newIV = new int[16]; //16 = IV size
+        for (int i=0; i<16-transmissionSize; i++) {
+            newIV[i] = previousEncrIV.get(i+transmissionSize);//Get Least significant bits (to the right)
+            //System.out.print(String.format("%02X",newIV[i]) + " ");
+        }
+        for (int i=16-transmissionSize; i<16; i++) {
+            newIV[i] = text[i-(16-transmissionSize)]; 
+            //System.out.print(String.format("%02X",newIV[i]) + " ");
+        }
+
+        return newIV;
+    }
 
     }
 
